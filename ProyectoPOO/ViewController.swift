7342b4d1b6f09a8383da269df14bceb15d5ac3c4
C4rlos316 <cama619@gmail.com>
2023-01-13 17:17:53 -0600

    Initial Commit

---
+++ ProyectoPOO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoPOO
+//
+//  Created by Carlos on 13/01/23.
+//  Copyright © 2023 POO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
